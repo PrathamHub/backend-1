0f8acba790c908a3ac2a716cc4697cf21c014385
Pratham <prathambagrecha1430@gmail.com>
2024-02-21 13:39:10 +0530

    Done till Custom api and error handles

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import connectDB from './db/index.js'
 dotenv.config({
     path:'./env'
 })
-connectDB()
+connectDB().then(
+    ()=>{
+        app.listen(process.env.PORT||8000,()=>{
+            console.log(`Server is running on PORT ${process.env.PORT}`);
+        })
+    }
+).catch((err)=>{console.log("Mongo db connection failed!!!!1",err);})
 
 
 
